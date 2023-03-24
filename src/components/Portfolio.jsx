405c3e02896195e0817370c1316f10f357e3c6ef
TomasTinto1234 <tititinto@gmail.com>
2023-03-24 11:23:24 -0300

    ultimas actualizaciones

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,7 +3,8 @@ import imagen from "../img/pi-lookhouse.jpeg"
 import imagen1 from "../img/image.png"
 import imagen2 from "../img/pi-dogs.jpeg"
 import imagen3 from "../img/pi-videogames.jpeg"
-import imagen4 from "../img/pi-pokemons.jpeg"
+import imagen6 from "../img/imagen6.jpeg"
+import imagen5 from "../img/imagen5.jpeg"
 
 
 const Portfolio = () => {
@@ -14,17 +15,33 @@ const Portfolio = () => {
    <div className="fila">
      <div className="proyecto">
        <div className="overlay"></div>
-     <img src={imagen}  alt=""/>
+     <img src={imagen}  alt="lookhouse"/>
        <div className="info">
-           <h4><a href="https://look-house.vercel.app/" target="_blank">Grupal Proyect LookHouse</a></h4>
+           <h4><a href="https://look-house.vercel.app/" target="_blank">Proyect LookHouse</a></h4>
+           <p>Web Design</p>
+       </div>
+   </div>
+   <div className="proyecto">
+       <div className="overlay"></div>
+     <img src={imagen5}  alt="migueloviedo"/>
+       <div className="info">
+           <h4><a href="https://miguel-oviedo.com/" target="_blank">Proyect MiguelOviedo</a></h4>
            <p>Web Design</p>
        </div>
    </div>
    <div className="proyecto">
            <div className="overlay"></div>
-           <img src={imagen1} alt=""/>
+           <img src={imagen6} alt="venta-libre"/>
            <div className="info">
-               <h4><a href="https://e-commerce-1pwr.vercel.app/" target="_blank">Proyecto Individual E-commerce</a></h4>
+               <h4><a href="https://ventalibre-37qgud8y0-tomastinto1234.vercel.app/" target="_blank">Proyect venta-libre</a></h4>
+               <p>Web Design</p>
+           </div>
+       </div>
+   <div className="proyecto">
+           <div className="overlay"></div>
+           <img src={imagen1} alt="e-commerce"/>
+           <div className="info">
+               <h4><a href="https://e-commerce-1pwr.vercel.app/" target="_blank">Proyect E-commerce</a></h4>
                <p>Web Design</p>
            </div>
        </div>
@@ -32,26 +49,18 @@ const Portfolio = () => {
            <div className="overlay"></div>
            <img src={imagen2} alt=""/>
            <div className="info">
-               <h4><a href="https://dogs-individual-proyect.vercel.app/" target="_blank"> Individual Proyect Dogs</a></h4>
+               <h4><a href="https://dogs-individual-proyect.vercel.app/" target="_blank">Proyect Dogs</a></h4>
                <p>Web Design</p>
            </div>
        </div>
-       <div className="proyecto">
+       {/* <div className="proyecto">
            <div className="overlay"></div>
-           <img src={imagen3} alt=""/>
+           <img src={imagen3} alt="videogames"/>
            <div className="info">
-               <h4> <a href="https://videogames-individual-proyect.vercel.app/" target="_blank"> Individual Proyect Videogames</a></h4>
+               <h4> <a href="https://videogames-individual-proyect.vercel.app/" target="_blank">Proyect Videogames</a></h4>
                <p>Web Design</p>
            </div>
-       </div>
-       <div className="proyecto">
-           <div className="overlay"></div>
-           <img src={imagen4} alt=""/>
-           <div className="info">
-               <h4> <a href="https://pokemon-individual-proyect.vercel.app/" target="_blank"> Individual Proyect Pokemons</a></h4>
-               <p>Web Design</p>
-           </div>
-       </div>
+       </div> */}
    </div>
   </section>
     </div>
