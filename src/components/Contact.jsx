4f8b37dd1bc397685676a213d0d2cb8d26d3e0d3
TomasTinto1234 <tititinto@gmail.com>
2023-04-18 13:24:46 -0300

    more details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Contact = () => {
   return (
     <section id="contacto" >
-    <h3 className="titulo-seccion"><a href="https://wa.me/+541161263966" className='as2' target="_blank">Contact me now<i className="fa-brands fa-whatsapp"></i></a> <a href="https://wa.me/1161263966"></a></h3>
+    <h3 className="titulo-seccion"><a href="https://wa.me/+541161263966" className='github' target="_blank">Contact me now<i className="fa-brands fa-whatsapp"></i></a> <a href="https://wa.me/1161263966"></a></h3>
     <div className="contenedor-form">
         <form action="">
-        <button   value="Enviar Mensaje" className="btn-enviar"><a href="https://wa.me/+541161263966" target="_blank">Send Message</a></button>
+        <button   value="Enviar Mensaje" className="btn-enviar"><a href="https://wa.me/+541161263966" target="_blank" className='github'>Send Message</a></button>
         </form>
     </div>
 </section>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,6 +6,9 @@ import imagen3 from "../img/pi-videogames.jpeg"
 import imagen6 from "../img/imagen6.jpeg"
 import imagen5 from "../img/imagen5.jpeg"
 import imagen7 from "../img/half2.jpeg"
+import imagen8 from "../img/calculadora.jpeg"
+import imagen9 from "../img/ToDoList.jpeg"
+import { Link } from 'react-router-dom'
 
 
 
@@ -13,7 +16,7 @@ const Portfolio = () => {
   return (
     <div className='contenedor-skills4'>
          <section id="portfolio">
-   <h3 className="titulo-seccion">My Projects<a href="https://github.com/TomasTinto1234" target="_blank" > <i className="fa-brands fa-github"></i></a></h3>
+   <h3 className="titulo-seccion">My Projects<a href="https://github.com/TomasTinto1234" target="_blank" className='github' > <i className="fa-brands fa-github"></i></a></h3>
    <div className="fila">
    <div className="proyecto">
        <div className="overlay"></div>
@@ -43,7 +46,7 @@ const Portfolio = () => {
            <div className="overlay"></div>
            <img src={imagen6} alt="venta-libre"/>
            <div className="info">
-               <h4><a href="https://ventalibre-37qgud8y0-tomastinto1234.vercel.app/" className='as' target="_blank">venta-libre</a></h4>
+               <h4><a href="https://ventalibre-37qgud8y0-tomastinto1234.vercel.app/" className='as' target="_blank">Venta-Libre</a></h4>
                <p>Web Design</p>
            </div>
        </div>
@@ -55,14 +58,25 @@ const Portfolio = () => {
                <p>Web Design</p>
            </div>
        </div>
-       {/* <div className="proyecto">
+       <div className="proyecto">
            <div className="overlay"></div>
-           <img src={imagen2} alt=""/>
-           <div className="info" >
-               <h4><a href="https://dogs-individual-proyect.vercel.app/" className='as' target="_blank">Dogs</a></h4>
+           <img src={imagen8} alt="calculadora"/>
+           <div className="info">
+               <h4><a href="https://calculadora-403qu6863-tomastinto1234.vercel.app/" className='as' target="_blank">Calculadora</a></h4>
                <p>Web Design</p>
            </div>
-       </div> */}
+       </div>
+       <div className="proyecto">
+           <div className="overlay"></div>
+           <img src={imagen9} alt="ToDo-List"/>
+           <div className="info" >
+               <h4><a href="https://todo-list-837qo7rqw-tomastinto1234.vercel.app/" className='as' target="_blank">ToDo List</a></h4>
+               <p>Web Design</p>
+           </div>
+       </div>
+   </div>
+   <div className='center'>
+   <button   value="GitHub" className="btn-GitHub"><a href="https://github.com/TomasTinto1234?tab=repositories" target="_blank" className='github'>GitHub</a></button>
    </div>
   </section>
     </div>
