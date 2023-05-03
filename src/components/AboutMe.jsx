2d373efca533ff1fb321dedf8712d9d8c370bccc
TomasTinto1234 <tititinto@gmail.com>
2023-05-03 11:34:13 -0300

    about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -13,7 +13,7 @@ const AboutMe = () => {
           <h2>Hello, I am <span>Tomas Tinto</span> </h2>
           <h3>Web Developer</h3>
           <p>I’m a developer with experience in building websites for small and medium sized businesses. Whether you’re trying to win work, list your services or even create a whole online store – I can help! Regular communication is really important to mee, so let’s keep in touch</p>
-          <a href="https://www.canva.com/design/DAFUFxMEEhA/5W3wBZtjwLUdb3ido3fquQ/view?utm_content=DAFUFxMEEhA&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank">Download CV</a>
+          <a href="https://www.canva.com/design/DAFhSzLta6U/841xjgSAsj-n_1TTneNayw/view?utm_content=DAFhSzLta6U&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank">Download CV</a>
       </div>
   </section>
     </div>
