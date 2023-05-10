b58d0daea6aaba9051049a1e1ef373a18ac821cd
TomasTinto1234 <tititinto@gmail.com>
2023-05-10 14:35:12 -0300

    more details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <section id="contacto" >
+    <section  className='contenedor-skills2' id="contacto" >
     <h3 className="titulo-seccion"><a href="https://wa.me/+541161263966" className='github' target="_blank">Contact me now<i className="fa-brands fa-whatsapp"></i></a> <a href="https://wa.me/1161263966"></a></h3>
     <div className="contenedor-form">
         <form action="">
